6ae807ab3e3f00447081f86fe6ec2a14c6e11687
Anushka Ashirgade <ashir.anushka@gmail.com>
2025-02-08 17:41:13 -0500

    implementing barcode scanning

--- app/index.js
+++ app/index.js
@@ -1,15 +1,13 @@
-import React from "react";
-import * as eva from "@eva-design/eva";
-import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import { View, Text, Button } from "react-native";
+import { useRouter } from "expo-router";
 
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+export default function HomeScreen() {
+  const router = useRouter();
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to Ambrosia!</Text>
+      <Button title="Scan Food" onPress={() => router.push("/scan")} />
+    </View>
+  );
+}
